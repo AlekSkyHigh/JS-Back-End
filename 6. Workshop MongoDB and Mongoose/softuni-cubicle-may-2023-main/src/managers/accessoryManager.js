cf1ac40098fa68ffd61f73ee1ca69f26bf3526c9
Aleksandar Voynov <110685565+AlekSkyHigh@users.noreply.github.com>
2023-06-21 17:17:36 +0300

    Add files via upload

---
+++ 6. Workshop MongoDB and Mongoose/softuni-cubicle-may-2023-main/src/managers/accessoryManager.js
@@ -0,0 +1,7 @@
+const Accessory = require('../models/Accessory');
+
+exports.getAll = () => Accessory.find();
+
+exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getOthers = (accessoryIds) => Accessory.find({ _id: { $nin: accessoryIds } });
